2ef9b1612b7869a131bb9a818c9a6b56d927a6ac
OzadovZohidjon <zohid.zokirivich2000@gmail.com>
2021-10-03 20:19:17 +0500

    responsive

---
+++ js/app.js
@@ -0,0 +1,59 @@
+$('.gallery__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: '.gallery__slider-arrow__next',
+    prevArrow: '.gallery__slider-arrow__prev',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 568,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+$('.partners__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: '.partners__slider-arrow__next',
+    prevArrow: '.partners__slider-arrow__prev',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 568,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
