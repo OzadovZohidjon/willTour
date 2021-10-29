88c3ea7237dac134fc669f92e7ae590cd12490b0
unknown <zohid.zokirivich2000@gmail.com>
2021-10-29 18:38:30 +0500

    change responsive

--- js/app.js
+++ js/app.js
@@ -33,11 +33,47 @@ $('.gallery__slider').slick({
     ]
 });
 
-$('.partners__slider').slick({
+// $('.partners__slider').slick({
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     nextArrow: '.partners__slider-arrow__next',
+//     prevArrow: '.partners__slider-arrow__prev',
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 4,
+//                 slidesToScroll: 1,
+//                 dots: true
+
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1,
+//                 dots: true
+
+//             }
+//         },
+//         {
+//             breakpoint: 568,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 1,
+//                 dots: true
+
+//             }
+//         }
+//     ]
+// });
+
+$('.partners__slider-other').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    nextArrow: '.partners__slider-arrow__next',
-    prevArrow: '.partners__slider-arrow__prev',
+    nextArrow: '.partners__slider-other-arrow__next',
+    prevArrow: '.partners__slider-other-arrow__prev',
     responsive: [
         {
             breakpoint: 1024,
